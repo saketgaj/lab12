ac2668a6b68794c51e698574d758c0070f3b158c
Saket Gajavada <saketgaj@gmail.com>
2023-11-17 12:02:14 -0800

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -33,6 +33,10 @@ public class FamilyTree
         {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+        	
+        	this.children.add(childNode);
+        	childNode.parent = this;
+        	
         }
         
         
@@ -41,12 +45,15 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
+            	if(child.getNodeWithName(targetName) != null) {
+            		return child;
+            	}
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
             }
@@ -66,6 +73,12 @@ public class FamilyTree
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
+            
+            TreeNode temp = this;
+            while(temp.parent != null) {
+            	ancestors.add(temp.parent);
+            	temp = temp.parent;
+            }
 
             return ancestors;
         }
